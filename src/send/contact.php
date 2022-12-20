1698e14dc8dc2b888a9b29932167464c8678eae9
janko8403 <janko8403@gmail.com>
2022-12-20 15:46:05 +0100

    init remve

--- src/send/contact.php
+++ src/send/contact.php
@@ -45,14 +45,14 @@
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true; 
-        $mail->Username = 'prudensolve1234@gmail.com';
-        $mail->Password = 'prudensolve1234!@#$';
+        $mail->Username = '';
+        $mail->Password = '';
         $mail->SMTPSecure = 'tls';                          
         $mail->Port = 587;
 
-        $mail->setFrom('hello@arbridge.pl', 'AR Bridge');
-        $mail->addAddress('janko8403@gmail.com');
-        $mail->addAddress('hello@arbridge.pl');
+        $mail->setFrom('l', 'AR Bridge');
+        $mail->addAddress('');
+        $mail->addAddress('');
 
         $mail->isHTML( true );
         $mail->CharSet = "utf-8";
